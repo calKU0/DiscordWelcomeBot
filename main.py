84740592f8b04838da480eea7bc7879754c33f17
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2023-01-16 11:29:42 +0100

    Roll command implemented

---
+++ main.py
@@ -0,0 +1,27 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+import random
+
+TOKEN = "MTA2MzQ1MTY2NzU5MDMwMzc5NA.GaTcEq.pwW2q3XBOYbDItjbhesdynJQEU4UPZqWcqDBp8"
+bot = commands.Bot(command_prefix=">", intents = discord.Intents.all()) 
+
+@bot.event
+async def on_ready():
+    print("We've logged in as {0.user}".format(bot))
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+
+@bot.tree.command(name="roll", description="Rolls a random number")
+@app_commands.choices(choices=[app_commands.Choice(name="100",value="100"), app_commands.Choice(name="10",value="10")])
+async def roll(interaction: discord.Interaction, choices:app_commands.Choice[str]):
+    if choices.value == "10":
+        await interaction.response.send_message(f"Rolled: {random.randint(1,10)}")
+    else:
+        await interaction.response.send_message(f"Rolled: {random.randint(1,100)}")
+    
+bot.run(TOKEN)
